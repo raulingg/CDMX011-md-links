f0fac0e85676f4c9cc1d462efa9f990d15c2d830
AngelicaJH <isc.florjardinez@gmail.com>
2021-10-13 09:31:32 -0500

    se instancio comportamiento para cuando el path sea de file o carpeta y se pueda ejecutar la logica desde el index

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-module.exports = () => {
-  // ...
-};
+//consideracion si se pasa una carpeta no debe tener espacios su nombre
+// import fs from 'fs';
+import path from 'path';
+import {toReadFile} from './libs/file.js'
+import {absolutePath} from './libs/ruta.js'
+import {toReadDirectory} from './libs/directory.js'
+
+
+const pathCli=absolutePath(process.argv[2]);
+
+
+
+if(path.extname(pathCli).length > 0){
+    // console.log('es un file')
+    // path.extname(pathCli) == '.txt'? toReadFile(pathCli) : console.log('Extencion de archivo no valida')
+    toReadFile(pathCli)
+
+  }else{
+    // console.log(path.basename(pathCli))
+    toReadDirectory(pathCli);
+  }
+
+
+  export const mdLink=()=>{
+
+  }
