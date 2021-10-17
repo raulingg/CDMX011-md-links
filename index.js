d09b141f33e52b0319de86ab531d461dd70588fc
AngelicaJH <isc.florjardinez@gmail.com>
2021-10-16 23:27:44 -0500

    se modifico la estructura y se implemento stats de node

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //consideracion si se pasa una carpeta no debe tener espacios su nombre
-// import fs from 'fs';
-import path from 'path';
+import fs from 'fs';
 import {toReadFile} from './libs/file.js'
 import {absolutePath} from './libs/ruta.js'
 import {toReadDirectory} from './libs/directory.js'
@@ -9,17 +8,20 @@ import {toReadDirectory} from './libs/directory.js'
 const pathCli=absolutePath(process.argv[2]);
 
 
-
-if(path.extname(pathCli).length > 0){
-    // console.log('es un file')
-    // path.extname(pathCli) == '.txt'? toReadFile(pathCli) : console.log('Extencion de archivo no valida')
-    toReadFile(pathCli)
-
-  }else{
-    // console.log(path.basename(pathCli))
-    toReadDirectory(pathCli);
-  }
-
+   fs.stat(pathCli, (err, stats)=>{
+    if(err){
+        return console.log(err.message);
+    }
+  
+    if(stats.isFile()){
+      console.log('file')
+      toReadFile(pathCli)
+    }else if(stats.isDirectory()){
+      // console.log(path.basename(pathCli))
+     toReadDirectory(pathCli);
+    }
+  
+  });
 
   export const mdLink=()=>{
 
